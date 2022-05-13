7c37528bd97fd91a559f77ceca6a785389431cda
prasad <88613962+prasad-78@users.noreply.github.com>
2022-05-13 16:44:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-const express = require('express')
+import express from 'express';
 const app = express()
-const port = 3000
+const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(process.env.PORT, '0.0.0.0', () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(port, '0.0.0.0', () => {
+	console.log(`Example app listening on port ${port}`)
+	console.log(`URI ${process.env.ATLAS_LAMBDA_URI}`)
 })
