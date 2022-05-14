dd34ea9b9e0e0f561c1d4097c36bcbc5a36901d5
prasad <88613962+prasad-78@users.noreply.github.com>
2022-05-14 12:37:34 +0530

    Add Controller

---
+++ app/controllers/getRecipesController.js
@@ -0,0 +1,3 @@
+export default function (req, res) {
+	res.send([{ name: 'pulihora' }, { name: 'khaza' }]);
+}
--- server.js
+++ server.js
@@ -1,13 +1,27 @@
-import express from 'express';
+import express from "express";
 const app = express();
 const port = process.env.PORT || 8080;
+import mongoose from "mongoose";
+import getRecipeController from "./app/controllers/getRecipeController.js";
+import getRecipesController from "./app/controllers/getRecipesController.js";
+import postRecipeController from "./app/controllers/postRecipeController.js";
+import patchRecipeController from "./app/controllers/patchRecipeController.js";
+import deleteRecipeController from "./app/controllers/deleteRecipeController.js";
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const MONGO_URI = process.env.ATLAS_URI;
 
-app.listen(port, '0.0.0.0', () => {
-	console.log(`Example app listening on port ${port}`)
-})
+app.use(express.json());
 
+app.get("/recipes", getRecipesController);
 
+app.post("/recipe", postRecipeController);
+
+app.get("/recipe/:id", getRecipeController);
+
+app.patch("/recipe/:id", patchRecipeController);
+
+app.delete("/recipe/:id", deleteRecipeController);
+
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Example app listening on port ${port}`);
+});
